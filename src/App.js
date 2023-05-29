7228fc0dbe2ca7784dcba23cfed7ec5bde3ef077
chrono234 <jairoor01outlook.com>
2023-05-28 19:09:32 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./NavBar";
+import HomePage from "C:/Users/jaior/OneDrive/Escritorio/Nuevos proyectos/my-blog/src/pages/HomePage.js";
+import AboutPage from "C:/Users/jaior/OneDrive/Escritorio/Nuevos proyectos/my-blog/src/pages/AboutPage.js";
+import ArticlesListPage from "./pages/ArticlesListPage";
+import ArticlePage from "./pages/ArticlePage";
+import NotFoundPage from "./pages/NotFoundPage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <NavBar>My awesome blog</NavBar>
+        <div id="page-body">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/articles" element={<ArticlesListPage />} />
+            <Route path="/articles/:articleId" element={<ArticlePage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,4 @@
+
+const NotFoundPage = () => <h1 className="error">404: Page Not Found!</h1>;
+
+export default NotFoundPage;
